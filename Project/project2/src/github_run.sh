fb70255d78efbbcd09b77a4e0a28e6044b047100
Leo_Adventure <12012919@mail.sustech.edu.cn>
2022-11-25 23:06:53 +0800

    finish project2-semantic analysis

---
+++ Project/project2/src/github_run.sh
@@ -0,0 +1,30 @@
+echo test1
+./bin/splc test/test_2_r01.spl > myOutput/output1.txt
+echo test2
+./bin/splc test/test_2_r02.spl > myOutput/output2.txt
+echo test3
+./bin/splc test/test_2_r03.spl > myOutput/output3.txt
+echo test4
+./bin/splc test/test_2_r04.spl > myOutput/output4.txt
+echo test5
+./bin/splc test/test_2_r05.spl > myOutput/output5.txt
+echo test6
+./bin/splc test/test_2_r06.spl > myOutput/output6.txt
+echo test7
+./bin/splc test/test_2_r07.spl > myOutput/output7.txt
+echo test8
+./bin/splc test/test_2_r08.spl > myOutput/output8.txt
+echo test9
+./bin/splc test/test_2_r09.spl > myOutput/output9.txt
+echo test10
+./bin/splc test/test_2_r10.spl > myOutput/output10.txt
+echo test11
+./bin/splc test/test_2_r11.spl > myOutput/output11.txt
+echo test12
+./bin/splc test/test_2_r12.spl > myOutput/output12.txt
+echo test13
+./bin/splc test/test_2_r13.spl > myOutput/output13.txt
+echo test14
+./bin/splc test/test_2_r14.spl > myOutput/output14.txt
+echo test15
+./bin/splc test/test_2_r15.spl > myOutput/output15.txt
---
+++ Project/project2/src/tree.cpp
@@ -0,0 +1,95 @@
+#include "tree.hpp"
+
+Node::Node(string name, int nodes_num, int line_num, ...) : nodetype(NONTERMINAL), name(name), nodes_num(nodes_num), line_num(line_num)
+{
+    va_list childnodes;
+    va_start(childnodes, line_num);
+    for (int i = 0; i < nodes_num; i++)
+    {
+        Node *node = (Node *)va_arg(childnodes, Node *);
+        this->nodes_queue.push(node);
+    }
+    va_end(childnodes);
+}
+
+Node::Node(NodeType nodetype, char *char_value, int line_num) : nodetype(nodetype), line_num(line_num)
+{
+    this->char_value = new char[100];
+    memcpy(this->char_value, char_value, strlen(char_value));
+}
+
+Node::Node(string name, int line_num) : nodetype(TERMINAL), name(name), line_num(line_num)
+{
+}
+
+Node::Node(NodeType nodetype, string name, int int_value, int line_num) : nodetype(nodetype), int_value(int_value), line_num(line_num)
+{
+    this->name = string(name);
+}
+
+Node::Node(NodeType nodetype, string name, float float_value, int line_num) : nodetype(nodetype), float_value(float_value), line_num(line_num)
+{
+    this->name = string(name);
+}
+
+void printTree(Node *root, int space)
+{
+    printf("%s (%d)\n", root->name.c_str(), root->line_num);
+    int nodes_num = root->nodes_num;
+    queue<Node *> nodes_queue = root->nodes_queue;
+    for (int i = 0; i < nodes_num; i++)
+    {
+        Node *node = nodes_queue.front();
+        print(node, space + 2);
+        nodes_queue.pop();
+    }
+}
+
+void print(Node *node, int space)
+{
+    if(node->nodes_num == 0 && node->nodetype == NONTERMINAL){
+        return;
+    }
+    for (int i = 0; i < space; i++)
+    {
+        printf(" ");
+    }
+    switch (node->nodetype)
+    {
+    case Type:
+    {
+        printf("TYPE: %s\n", node->char_value);
+        break;
+    }
+    case Int:
+    {
+        printf("INT: %s\n", node->name.c_str());
+        break;
+    }
+    case Char:
+    {
+        printf("CHAR: %s\n", node->char_value);
+        break;
+    }
+    case Float:
+    {
+        printf("FLOAT: %s\n", node->name.c_str());
+        break;
+    }
+    case Id:
+    {
+        printf("ID: %s\n", node->char_value);
+        break;
+    }
+    case TERMINAL:
+    {
+        printf("%s\n", node->name.c_str());
+        break;
+    }
+    case NONTERMINAL:
+    {
+        printTree(node, space);
+        break;
+    }
+    }
+}
